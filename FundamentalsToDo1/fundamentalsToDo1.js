3a2b8704439d4721414ebca235850c4a6781a61d
RyanHeishman <heishy0@gmail.com>
2023-09-22 10:21:32 -0700

    Algo Practice

---
+++ FundamentalsToDo1/fundamentalsToDo1.js
@@ -0,0 +1,49 @@
+// var myNumber = 42
+// var myName = 'Ryan'
+// var myNumber = 'Ryan'
+// var myName = 42
+
+// console.log('name: ' + myName)
+// console.log('number: ' + myNumber)
+
+//--------------------------------------------------
+
+// for (let i = -52; i <= 1066; i++) {
+//     console.log([i])
+// }
+
+//--------------------------------------------------
+
+// for (let i = -300; i < 0; i++) {
+//     if (i == -3 || i == -6){
+//         continue;
+//     }
+//     if (i % 3 == 0){
+//         console.log(i)
+//     }
+// }
+
+//--------------------------------------------------
+
+// i = 1999;
+
+// while (i < 5280) {
+//     i++;
+//     console.log(i)
+// }
+
+//--------------------------------------------------
+
+// const currentMonth = new Date().getMonth() + 1;
+// const currentDay = new Date().getDate();
+// const givenNumber1 = 22;
+// const givenNumber2 = 9;
+
+// if ((givenNumber1 === currentMonth && givenNumber2 === currentDay) || (givenNumber1 === currentDay && givenNumber2 === currentMonth)) {
+//     console.log('How did you know?')
+// }
+// else{
+//     console.log('Just another day...')
+// }
+
+//--------------------------------------------------
